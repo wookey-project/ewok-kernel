2a3f8e9f826cbacef1a55977f34eaf448d21b3fb
Philippe Thierry <phil@reseau-libre.net>
2019-07-09 11:30:16 +0200

    [update] add Ada application metainfo generation (out of ads template

--- tools/devmap/gen_app_metainfos.pl
+++ tools/devmap/gen_app_metainfos.pl
@@ -3,7 +3,7 @@
 
 use strict;
 # getting back script path
-use File::Basename qw(dirname);
+use File::Basename qw(dirname basename);
 use Cwd  qw(abs_path);
 # and update @INC
 use lib dirname(dirname abs_path $0) . '/devmap/lib';
@@ -40,17 +40,122 @@ my $mode     = shift;
 #   slotting/paging (depending on the current SoC).
 #
 
-my @applications = <"$builddir/apps/*/*.dummy.\L$mode\E.elf">;
+my @applines;
+my @applications = <"$builddir/apps/*/*.\L$mode\E.elf">;
+my $appid = 1;
 foreach my $application (@applications) {
+    next if ($application =~ m/.*\.dummy\.\L$mode\E.elf/);
     print("parsing ELF file: $application");
+
+    # 1) open the ELF file
     Devmap::Elfinfo::openelf($application);
+
+    # 2) get back the requested RAM and Flash size, using the ELF binary
     my $flash_size = get_flash_size();
     my $ram_size = get_ram_size();
-    print("flash size: $flash_size, ram_size: $ram_size");
+
     printf("flash size: 0x%X, ram_size: 0x%x\n", $flash_size, $ram_size);
+    print("flash size: $flash_size, ram_size: $ram_size");
+
+    # 3) Generate the arch-independent generic structure 'config.application_layout.ads
+
+    push @applines, create_app_generic_info($application, $appid);
+
+
+    # 4) Generate the arch-specific (MMU or MPU) structure 'config.(mpu|mpu).applications_layout.ads'
+    #    by now, we handle the MPU layout
+    #
+
+    $appid += 1;
+
+}
+
+foreach my $line (@applines) {
+    print($line);
+}
+
+
+#---
+# Utility functions
+#---
+
+sub hex_to_ada {
+    my $hexval = $_;
+
+
+}
+
+sub create_app_generic_info {
+    my $application = $_[0];
+    my $id = $_[1];
+    my $appname = basename($application,  ".\L$mode\E.elf");
+    my %appinfo = {
+        text_addr   => '0',
+        text_size   => '0',
+        data_addr   => '0',
+        data_size   => '0',
+        bss_size    => '0',
+        stack_size  => '0',
+    };
+
+    if (Devmap::Elfinfo::elf_section_exists('.text')) {
+        my %hash = Devmap::Elfinfo::elf_get_section('.text');
+        # then add its size
+        $appinfo{'text_addr'} = $hash{'lma'};
+        $appinfo{'text_size'} = $hash{'size'};
+    }
+    if (Devmap::Elfinfo::elf_section_exists('.data')) {
+        my %hash = Devmap::Elfinfo::elf_get_section('.data');
+        # then add its size
+        $appinfo{'data_addr'} = $hash{'lma'};
+        $appinfo{'data_size'} = $hash{'size'};
+    }
+    if (Devmap::Elfinfo::elf_section_exists('.bss')) {
+        my %hash = Devmap::Elfinfo::elf_get_section('.bss');
+        # then add its size
+        $appinfo{'bss_size'} = $hash{'size'};
+    }
+    if (Devmap::Elfinfo::elf_section_exists('.stacking')) {
+        my %hash = Devmap::Elfinfo::elf_get_section('.stacking');
+        # then add its size
+        $appinfo{'stack_size'} = $hash{'size'};
+    }
+    # formating as Ada hex format
+    foreach (keys %appinfo) {
+        $appinfo{$_} =~ s/0x(\d{4})(\d{4})/16#$1_$2#/;
+    }
+
+    $appname = "\U$appname\E";
+    my $appline = sprintf("   ID_APP%d => (%s_name, %s, %s, %s, %s, %s, %s),",
+    $id, $appname, $appinfo{'text_addr'}, $appinfo{'text_size'},
+    $appinfo{'data_addr'}, $appinfo{'data_size'},$appinfo{'bss_size'},
+    $appinfo{'stack_size'});
+
+    return $appline;
+}
+
+sub get_section_size {
+    if (Devmap::Elfinfo::elf_section_exists($_)) {
+        my %hash = Devmap::Elfinfo::elf_get_section($_);
+        return hex($hash{'size'});
+    }
+    return 0;
+}
+
+sub get_section_lma {
+    if (Devmap::Elfinfo::elf_section_exists($_)) {
+        my %hash = Devmap::Elfinfo::elf_get_section($_);
+        return hex($hash{'lma'});
+    }
+    return 0;
 }
 
 # get flash size for current application
+#
+# @argument:     None
+# @prerequisite: Devmap::Elfinfo::openelf() must have been previously called
+# @return:       the flash requested size in bytes
+#
 sub get_flash_size {
     my %hash;
     my $size = 0;
@@ -66,6 +171,12 @@ sub get_flash_size {
     return $size;
 }
 
+# get the ram size of the current application
+#
+# @argument:     None
+# @prerequisite: Devmap::Elfinfo::openelf() must have been previously called
+# @return:       the RAM requested size in bytes
+#
 sub get_ram_size {
     my %hash;
     my $size = 0;
@@ -81,12 +192,7 @@ sub get_ram_size {
     return $size;
 }
 
-#---
-# Utility functions
-#---
-
-sub usage()
-{
+sub usage {
   print STDERR "usage: $0  <build_dir> <mode>";
   exit(1);
 }
